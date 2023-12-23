d9879e94ba84797afbcebad048d98d177f805bd5
akshaykamble04 <akshaykamble1431@gmail.com>
2023-12-23 11:57:55 +0530

    updated Cart entity and CartItem entity completed

--- src/main/java/com/app/entities/Cart.java
+++ src/main/java/com/app/entities/Cart.java
@@ -13,8 +13,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import lombok.Data;
 
 @Entity
+@Data
 public class Cart {
 	
 	@Id
@@ -28,5 +30,14 @@ public class Cart {
 	@OneToMany(mappedBy = "cart",cascade = CascadeType.ALL,orphanRemoval = true)
 	@Column(name = "cart_items")
 	private Set<CartItem> cartItems = new HashSet<>();
+	
+	@Column(name = "total_price")
+	private double totalPrice;
+	
+	private int totalItem;
+	
+	private int totalDiscountedPrice;
+	
+	private int discount;
 
 }
